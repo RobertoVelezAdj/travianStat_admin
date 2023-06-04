8137c71a52e7eee39c74ed9adb5e27b098d32bbb
RobertoVelezAdj <roberto-velez@outlook.com>
2023-06-04 11:20:25 +0200

    1

--- app/Http/Controllers/Controller_vacas.php
+++ app/Http/Controllers/Controller_vacas.php
@@ -44,6 +44,7 @@ class Controller_vacas extends Controller
             $query = "select a.id as id_aldea, ina.idAldea as aldeaVaca, ina.coord_x, ina.coord_y, ina.poblacion, ina.NombreAldea, s.id as idServer from aldea_inac ina, servidor s, users u, aldea a where a.id_usuario = u.id and ina.NombreAldea = '".trim($cadena[$nombre_aldea])."' and calcular_distancia(a.coord_x,a.coord_y,ina.coord_x,ina.coord_y) = ".$cadena[$distancia]." and u.servidor = s.id and s.fch_mod = ina.created_at and a.id  =".$info->id_aldea;
             $resultado= DB::select($query);
             foreach ($resultado as $a){
+                echo "hola mundo1";
             //se mira si esta en listas de vacas y si no... se inserta
                 $query2 = "SELECT * FROM lista_vacas l where l.IdAldea = ".$a->id_aldea." and l.IdServer = ".$a->idServer." and l.IdAldeaVaca = ".$a->aldeaVaca;
                 $resultado2= DB::select($query2);
@@ -67,7 +68,7 @@ class Controller_vacas extends Controller
             $result = $result+6; 
         }
 
-       // $aux=$this->creacion_mensaje('success', "Vacas almacenadas de forma correcta.",$idUsu);
+        $aux=$this->creacion_mensaje('success', "Vacas almacenadas de forma correcta.",$idUsu);
        // return redirect()->action('App\Http\Controllers\Controller_vacas@inicio');
     }
 
