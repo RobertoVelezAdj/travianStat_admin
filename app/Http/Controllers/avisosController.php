e7d11e15eb0134dc3d9c468b7edbca510ba1f990
RobertoVelezAdj <roberto-velez@outlook.com>
2023-06-20 22:57:34 +0200

    sad

--- app/Http/Controllers/avisosController.php
+++ app/Http/Controllers/avisosController.php
@@ -27,8 +27,7 @@ class avisosController extends Controller
             $id_chat = $a->id_chat;
             $texto = $a->texto;
             $link = $a->link;
-            echo $mensaje."/".$id_notificacion;
-            $mensaje = new mensaje ([
+             $mensaje = new mensaje ([
                 'telegramid' => $id_chat,
                 'texto' =>$texto,
                 'link' => $link ,
